8ec633dcf5aac3a75394fb63c16b6b06898921d4
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-08-31 19:41:44 -0700

    Update Graphics.h

--- src/Graphics.h
+++ src/Graphics.h
@@ -9,8 +9,7 @@
 class Graphics
 {
 public:
-    // constructor / desctructor
-
+    
     // getters / setters
     void setBgFilename(std::string filename) { _bgFilename = filename; }
     void setTrafficObjects(std::vector<std::shared_ptr<TrafficObject>> &trafficObjects) { _trafficObjects = trafficObjects; };
