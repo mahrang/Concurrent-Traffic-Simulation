ebaf9cf9450b37ef2404d1d41cf017cc85825c64
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-08-31 20:13:37 -0700

    Refactor comments in TrafficLight.h for clarity

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -10,12 +10,12 @@
 class Vehicle;
 
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive.
-// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type.
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase.
-// Also, there should be an std::condition_variable as well as an std::mutex as private members.
+// Defined a class "MessageQueue" which has the public methods send and receive.
+// Send takes an rvalue reference of type TrafficLightPhase whereas receive returns this type.
+// Also, the class defines an std::dequeue called _queue, which stores objects of type TrafficLightPhase.
+// Also, there is an std::condition_variable as well as an std::mutex as private members.
 
-// original function header from starter code.  T represents TrafficLightPhase
+// T represents TrafficLightPhase
 template <class T>
 class MessageQueue
 {
@@ -29,11 +29,11 @@ private:
   std::mutex _mutex;
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject.
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“.
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value.
+// Defined a class "TrafficLight“ which is a child class of TrafficObject.
+// The class has the public methods "void waitForGreen()" and "void simulate()"
+// as well as "TrafficLightPhase getCurrentPhase()", where TrafficLightPhase is an enum that
+// can be either "red" or "green". Also, I added the private method "void cycleThroughPhases()".
+// Furthermore, there is the private member _currentPhase which can take "red" or "green" as its value.
 
 // TrafficLightPhase is an enum that can be either red or green
 enum TrafficLightPhase {red, green};
@@ -54,8 +54,8 @@ private:
     // typical behaviour methods
   void cycleThroughPhases();
   TrafficLightPhase _currentPhase;
-    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase
-    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling
+    // Created a private member of type MessageQueue for messages of type TrafficLightPhase
+    // and used it within the infinite loop to push each new TrafficLightPhase into it by calling
     // send in conjunction with move semantics.
   MessageQueue<TrafficLightPhase> _msg;
 };
