26988d7103b9b8bca2bbbf617d85dca6d9e2d30d
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-08-31 20:22:11 -0700

    Update TrafficLight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -6,14 +6,13 @@
 #include <condition_variable>
 #include "TrafficObject.h"
 
-// forward declarations to avoid include cycle
+// forward declaration to avoid include cycle
 class Vehicle;
 
 
 // Defined a class "MessageQueue" which has the public methods send and receive.
 // Send takes an rvalue reference of type TrafficLightPhase whereas receive returns this type.
 // Also, the class defines an std::dequeue called _queue, which stores objects of type TrafficLightPhase.
-// Also, there is an std::condition_variable as well as an std::mutex as private members.
 
 // T represents TrafficLightPhase
 template <class T>
@@ -29,16 +28,10 @@ private:
   std::mutex _mutex;
 };
 
-// Defined a class "TrafficLight“ which is a child class of TrafficObject.
-// The class has the public methods "void waitForGreen()" and "void simulate()"
-// as well as "TrafficLightPhase getCurrentPhase()", where TrafficLightPhase is an enum that
-// can be either "red" or "green". Also, I added the private method "void cycleThroughPhases()".
-// Furthermore, there is the private member _currentPhase which can take "red" or "green" as its value.
-
 // TrafficLightPhase is an enum that can be either red or green
 enum TrafficLightPhase {red, green};
 
-// TrafficLight class is a child class of TrafficObject
+// Class TrafficLight is a child class of TrafficObject
 class TrafficLight : public TrafficObject
 {
 public:
@@ -53,7 +46,7 @@ public:
 private:
     // typical behaviour methods
   void cycleThroughPhases();
-  TrafficLightPhase _currentPhase;
+  TrafficLightPhase _currentPhase;  // _currentPhase takes "red" or "green" as its value
     // Created a private member of type MessageQueue for messages of type TrafficLightPhase
     // and used it within the infinite loop to push each new TrafficLightPhase into it by calling
     // send in conjunction with move semantics.
