5cca60a5f15c2686de674159c30b24f252228196
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-08-31 19:55:36 -0700

    Fix formatting of comments and member variables

--- src/Intersection.h
+++ src/Intersection.h
@@ -52,9 +52,9 @@ private:
     void processVehicleQueue();
 
     // private members
-    std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
+    std::vector<std::shared_ptr<Street>> _streets;  // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
-    bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
+    bool _isBlocked;  // flag indicating wether the intersection is blocked by a vehicle
     TrafficLight _trafficLight; 
 };
 
