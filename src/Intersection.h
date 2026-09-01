c87893917682233c8b9a8901230a6295d15465b8
Mahrang Saeed <79680735+mahrang@users.noreply.github.com>
2026-08-31 19:54:55 -0700

    Fix formatting in Intersection.h

--- src/Intersection.h
+++ src/Intersection.h
@@ -24,7 +24,7 @@ public:
     void permitEntryToFirstInQueue();
 
 private:
-    std::vector<std::shared_ptr<Vehicle>> _vehicles;          // list of all vehicles waiting to enter this intersection
+    std::vector<std::shared_ptr<Vehicle>> _vehicles;  // list of all vehicles waiting to enter this intersection
     std::vector<std::promise<void>> _promises; // list of associated promises
     std::mutex _mutex;
 };
@@ -55,7 +55,7 @@ private:
     std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
     bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
-    TrafficLight _trafficLight;  // for FP.6a
+    TrafficLight _trafficLight; 
 };
 
 #endif
